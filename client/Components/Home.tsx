5732dd79ff5592d8d35c8ac43ad6a09d30e03cd5
Harsh Barnwal <barnwal301harsh@gmail.com>
2026-04-10 12:26:19 +0530

    added files for vercel frontend

--- client/Components/Home.tsx
+++ client/Components/Home.tsx
@@ -6,7 +6,7 @@ import CustomerProductCard from "./CustomerProductCard";
 import axios from "axios";
 
 const images = [
-  "/banners/1.jpg",
+  "/public/banners/1.jpg",
   "/banners/2.jpg",
   "/banners/3.jpg",
   "/banners/4.jpg",
